09b4e3333a1498de2c058ffa46af94b095eceb48
jobum97 <brian654321@naver.com>
2022-01-27 12:16:23 +0900

    issue#2 까지 구현

--- savePenguin/app/src/main/java/com/example/savepenguin/MainActivity.java
+++ savePenguin/app/src/main/java/com/example/savepenguin/MainActivity.java
@@ -8,12 +8,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import com.example.savepenguin.account.SharedPreference;
 
 public class MainActivity extends AppCompatActivity {
 
     private PenguinFragement penguinFragement;
 
     private Button QR_menuBtn;
+    private TextView text_userId;
+    private String userID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,8 +26,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Log.v("메인 페이지", "메인 Activity 시작");
         penguinFragement = new PenguinFragement();
+        userID = SharedPreference.getAttribute(getApplicationContext(), "userid");
 
         getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainerView_main, penguinFragement).commit();
+
+        text_userId = findViewById(R.id.text_userid);
+        text_userId.setText("ID : "+userID);
 
         QR_menuBtn = findViewById(R.id.QR_MenuBtn);
         QR_menuBtn.setOnClickListener(new View.OnClickListener() {
