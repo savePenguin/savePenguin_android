b5bf9d3891a4f56726795a0c770c7ad078720538
jobum97 <brian654321@naver.com>
2022-01-30 13:27:28 +0900

    구현ing

---
+++ savePenguin/app/src/main/java/com/example/savepenguin/ipSetting.java
@@ -0,0 +1,13 @@
+package com.example.savepenguin;
+
+public class ipSetting {
+    private String ipv4 = "192.168.219.111";
+
+    public String getIpv4() {
+        return ipv4;
+    }
+
+    public void setIpv4(String ipv4) {
+        this.ipv4 = ipv4;
+    }
+}
