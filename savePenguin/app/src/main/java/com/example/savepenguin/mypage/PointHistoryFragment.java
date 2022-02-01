34b05efab062685fcea6d9a0271b51ddcb30940a
jobum97 <brian654321@naver.com>
2022-02-01 15:03:36 +0900

    구현ing

--- savePenguin/app/src/main/java/com/example/savepenguin/mypage/PointHistoryFragment.java
+++ savePenguin/app/src/main/java/com/example/savepenguin/mypage/PointHistoryFragment.java
@@ -7,17 +7,28 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.savepenguin.Point;
+import com.example.savepenguin.QR;
 import com.example.savepenguin.R;
-import com.example.savepenguin.mypage.MyPageActivity;
+
+import java.util.ArrayList;
 
 
 public class PointHistoryFragment extends Fragment {
 
     MyPageActivity myPageActivity;
+    RecyclerView pointList;
+    LinearLayoutManager linearLayoutManager;
+    PointListAdapter adapter;
+    ArrayList<Point> items = new ArrayList<>();
 
     @Override
     public void onAttach(@NonNull Context context) {
@@ -49,7 +60,55 @@ public class PointHistoryFragment extends Fragment {
                 myPageActivity.onChangeFragment(0);
             }
         });
+        pointList = viewGroup.findViewById(R.id.ListView_QRList);
 
+
+        for (int i = 1; i < 6; i++) {
+            items.add(new Point(10 * i, "2022.01.01", "스타벅스 여의도역점", "내 컵"));
+        }
+
+        linearLayoutManager = new LinearLayoutManager(getActivity(), RecyclerView.VERTICAL, false);
+        adapter = new PointListAdapter(items);
+        pointList.setLayoutManager(linearLayoutManager);
+        pointList.setAdapter(adapter);
         return viewGroup;
+    }
+}
+
+class PointListAdapter extends RecyclerView.Adapter<PointListAdapter.Holder> {
+
+    ArrayList<Point> items = new ArrayList<>();
+
+    public PointListAdapter(ArrayList<Point> items) {
+        this.items = items;
+    }
+
+    @NonNull
+    @Override
+    public PointListAdapter.Holder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.recyclerview_pointhistory_item, parent, false);
+        return new PointListAdapter.Holder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull PointListAdapter.Holder holder, int position) {
+        Point item = items.get(position);
+        holder.pointName.setText("사용 텀블러 : " + item.getTumbler() + "        +" + item.getPoint() + "점");
+        holder.aboutPoint.setText("획득 날짜"+item.getPointDate()+" 획득 위치 : "+item.getPointFrom());
+    }
+
+    @Override
+    public int getItemCount() {
+        return items.size();
+    }
+
+    public class Holder extends RecyclerView.ViewHolder {
+        private TextView pointName;
+        private TextView aboutPoint;
+        public Holder(@NonNull View itemView) {
+            super(itemView);
+            pointName = itemView.findViewById(R.id.text_point_item);
+            aboutPoint = itemView.findViewById(R.id.text_aboutpoint);
+        }
     }
 }
